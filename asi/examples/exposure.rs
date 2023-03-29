88403f90fa07de8121299befa8aeaec6c7ac6f0e
LinuxHeki <linuxheki@gmail.com>
2023-03-29 16:47:04 +0200

    include bit depth in example

--- asi/examples/exposure.rs
+++ asi/examples/exposure.rs
@@ -12,6 +12,7 @@ fn main() {
     let width = camera_info.max_width;
     let height = camera_info.max_height;
     let color = camera_info.is_color_cam;
+    let bit_depth = camera_info.bit_depth > 1;
 
     let camera = Camera::open(camera_info.camera_id).unwrap();
 
@@ -24,16 +25,22 @@ fn main() {
     camera.start_exposure(false).unwrap();
     while camera.exposure_status().unwrap() == ExposureStatus::Working {}
 
-    let buffer_size = width*height*(1+2*color as u32);
+    let mut buffer_size = width*height;
+    if color {
+        buffer_size *= 3;
+    } else if bit_depth {
+        buffer_size *= 2;
+    }
+
     let mut data = vec![0; buffer_size as usize];
     camera.get_data_after_exposure(&mut data).unwrap();
     
-    let color = match color {
-        true => image::ColorType::Rgb8,
-        false => image::ColorType::L8,
+    let color = match (color, bit_depth) {
+        (true, _) => image::ColorType::Rgb8,
+        (false, true) => image::ColorType::L16,
+        (false, false) => image::ColorType::L8,
     };
 
     image::save_buffer("exposure.jpg", &data, width as u32, height as u32, color).unwrap();
-
     camera.close().unwrap();
 }
